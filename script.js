61da3e881f2e9cea89f6169d32d7a81fb7f741b0
yvesaur <yveschristianpcasio@gmail.com>
2023-09-04 01:52:40 +0800

    style: :lipstick: Set up HTML/CSS/JS Skeleton

---
+++ script.js
@@ -0,0 +1,8 @@
+const bookmarkList = [];
+
+function Book(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+}
