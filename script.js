214be7eb7655ad3a858f0c3fe5b0a6ca46dab1f5
yvesaur <yveschristianpcasio@gmail.com>
2023-09-07 01:38:47 +0800

    style: :lipstick: Finish UI for Bookmark list, Add Bookmark Modal

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const bookmarkList = [];
 
-function Book(title, author, pages, haveRead) {
+function Book(title, author, pages, year, haveRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.year = year;
     this.haveRead = haveRead;
+}
+
+function addBooktoLibrary() {
+     
 }
